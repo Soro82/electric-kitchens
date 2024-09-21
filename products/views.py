ecf069fbf1a944f9610750a62ff1374a8916b71f
Gary Sorohan <garysorohan@gmail.com>
2024-09-21 10:07:16 +0000

    Add category to product details

--- products/views.py
+++ products/views.py
@@ -49,7 +49,6 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
