3235c0cdc6e17dd8e252b2223de7f9bb73c7648f
Gary Sorohan <garysorohan@gmail.com>
2024-09-28 15:25:01 +0000

    Update category names and sizes in main nav

--- electric_kitchens/settings.py
+++ electric_kitchens/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = '4lcw&5-p!s!w3l8+e^u(hvzw2r)(f31qy2^f=%fs1e$urvsfyg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['8000-soro82-electrickitchens-ovc4otfkvhj.ws.codeinstitute-ide.net', 'electric-kitchens-02035ecbc37c.herokuapp.com']
 
