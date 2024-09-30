a3a2809650b8b7b67e42e0eef2381a7dbea86c5b
Gary Sorohan <garysorohan@gmail.com>
2024-09-30 14:19:51 +0000

    Add wishlist view and make migrations

--- electric_kitchens/settings.py
+++ electric_kitchens/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECRET_KEY = '4lcw&5-p!s!w3l8+e^u(hvzw2r)(f31qy2^f=%fs1e$urvsfyg'
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '4lcw&5-p!s!w3l8+e^u(hvzw2r)(f31qy2^f=%fs1e$urvsfyg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-soro82-electrickitchens-ovc4otfkvhj.ws.codeinstitute-ide.net', 'electric-kitchens-02035ecbc37c.herokuapp.com']
 
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from .models import Wishlist
+from products.models import Product
+from profiles.models import UserProfile
+
+
+@login_required
+def wishlist(request):
+    """
+    A view to display a user's Wishlist.
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.filter(user=profile)
+
+    return render(
+        request, 'wishlist/wishlist.html', {'wishlist': wishlist}
+    )
