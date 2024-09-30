816038c2868deb5d5dfe26428b84b3504ee0508d
Gary Sorohan <garysorohan@gmail.com>
2024-09-30 14:05:24 +0000

    Add wishlist app and model

---
+++ wishlist/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from profiles.models import UserProfile
+from products.models import Product
+
+
+class Wishlist(models.Model):
+    """
+    A Model to store a user's favourite items.
+    """
+    user = models.ForeignKey(UserProfile, null=False,
+                                blank=False, on_delete=models.CASCADE,
+                                related_name='wishlist')
+    product = models.ForeignKey(Product, null=False,
+                                blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
---
+++ wishlist/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.wishlist, name='wishlist'),
+    # path('add/<item_id>/', views.add_to_wishlist,
+    #      name='add_to_wishlist'),
+    # path('remove/<item_id>/', views.remove_from_wishlist,
+    #      name='remove_from_wishlist'),
+]
