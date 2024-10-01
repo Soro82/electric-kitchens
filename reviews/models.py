bfe9668a5d82c24b65d9c192be39dbbc809ad2d9
Gary Sorohan <garysorohan@gmail.com>
2024-10-01 10:01:09 +0000

    Add Review model to reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from profiles.models import UserProfile
+from products.models import Product
+
 # Create your models here.
+class Review(models.Model):
+    """ A model to store user's review information. """
+    user = models.ForeignKey(UserProfile, null=False,
+                                blank=False, on_delete=models.CASCADE,
+                                related_name='wishlist')
+    product = models.ForeignKey(Product, null=False,
+                                blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
